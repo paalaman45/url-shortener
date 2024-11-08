fe54aa45f667899ae73f43b447eb66e1281f8387
Kissel James Paalaman <kisseljamespaalam@gmail.com>
2024-11-08 08:16:41 +0800

    refactor: added copy clip board code

--- src/components/HomeForm.vue
+++ src/components/HomeForm.vue
@@ -3,11 +3,13 @@
     <div class="flex justify-center items-center h-screen">
       <div class="bg-white p-8 rounded-lg shadow-md w-1/3">
         <div class="flex items-center">
-          <input type="text" class="border border-gray-300 p-2 w-full rounded-l-lg" v-model="longUrl" placeholder="Enter Link" name="txt-url">
-          <button @click="shortenUrl" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg" name="btn-submit">Submit</button>
+          <input type="url" class="border border-gray-300 p-2 w-full rounded-l-lg" v-model="longUrl" placeholder="Enter Link" name="txt-url">
+          <button v-if="CopyButton" @click="copyToClipboard" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg" name="btn-submit">Copy</button>
+          <button :class="{'opacity-50 cursor-not-allowed': !isUrlValid, 'bg-blue-500 hover:bg-blue-700': isUrlValid}"  :disabled="!isUrlValid" v-else  @click="shortenUrl" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg" name="btn-submit">Submit</button>
         </div>
         <div class="items-center">
           <p class="pt-5 text-center" v-if="shortUrl">Shortened URL: <a :href="shortUrl">{{ shortUrl }}</a></p>
+          <p v-if="errorMessage" class="text-red-500 pt-5 text-center">{{ errorMessage }}</p>
         </div>
       </div>
     </div>
@@ -15,7 +17,7 @@
 </template>
 
 <script setup>
-  import { ref, onMounted } from 'vue';
+  import { ref, onMounted, watch, computed } from 'vue';
   import db from '../firebase/ini.js'
   import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
   import { useRoute } from 'vue-router';
@@ -23,28 +25,35 @@
 
   const longUrl = ref('');
   const shortUrl = ref('');
+  const CopyButton = ref(false);
+  const isUrlValid = computed(() => longUrl.value.startsWith('https'));
 
   // For Saving Data to Firebase
   const shortenUrl = async () => {
-    console.log('Long URL:', longUrl.value);
-    const shortID = Math.random().toString(36).substring(2, 7);
-    console.log("Link ID: " + shortID);
+    if(isUrlValid.value)
+    {
+      console.log('Long URL:', longUrl.value);
+      const shortID = Math.random().toString(36).substring(2, 7);
+      console.log("Link ID: " + shortID);
 
-    try {
-    const docRef = await addDoc(collection(db, 'links'), {
-      longUrl: longUrl.value,
-      shortID: shortID
-    });
-      console.log('Document written with ID:', docRef.id);
-      shortUrl.value = `${window.location.origin}/${shortID}`;
-    } catch (error) {
-      console.error('Error adding document:', error);
+      try {
+      const docRef = await addDoc(collection(db, 'links'), {
+        longUrl: longUrl.value,
+        shortID: shortID
+      });
+        console.log('Document written with ID:', docRef.id);
+        longUrl.value = `${window.location.origin}/${shortID}`;
+        CopyButton.value = true;
+      } catch (error) {
+        console.error('Error adding document:', error);
+      }
     }
   };
 
   // For Fetching URL Data
   const route = useRoute();
   const urlId = route.params.id;
+  const errorMessage = ref('');
   
   const getLink = async () => {
     try{
@@ -68,5 +77,18 @@
     if (urlId) {
       getLink();
     }
+  });
+
+  // CopyToClickboard
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(longUrl.value)
+    .then(() => alert('Copied to clickboard!'))
+  };
+
+  // Check Entries if have https
+  watch(longUrl, (newUrl) => {
+    errorMessage.value = newUrl && !newUrl.startsWith('https') 
+    ? 'URL must start with "https"' 
+    : '';
   });
 </script>
